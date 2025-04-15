21dce7b4c2d35c16fa021ef6df84276bba0044ba
HAYAT NABI <HAYATNABI@ISB.MTBC.COM>
2025-04-15 16:08:06 +0500

    Added css for Signup page and done it's error handling, Added Signup button to homepage and done it's horizontal alignment, Added Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if (@user.save)
+      flash[:notice] = "Welcome to  the Beta Blogs, " + @user.username << "! Your account has been created successyfully. Enjoy Blogging :)"
+      redirect_to articles_path # for now...
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
