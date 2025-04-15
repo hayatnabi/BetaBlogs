b22fad7cbab566f33ed78b32addf12686d244e32
HAYAT NABI <HAYATNABI@ISB.MTBC.COM>
2025-04-15 09:35:23 +0500

    Added password digest field column in users table and configured user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_14_094624) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_15_041205) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,5 +24,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_14_094624) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_hash"
+    t.string "password_digest"
   end
 end
